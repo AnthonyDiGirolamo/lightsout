a4e0a7d79507a425b50523d07066e55e33e916a3
AnthonyDiGirolamo <anthony.digirolamo@gmail.com>
2015-10-10 15:30:33 -0400

    added moonphase calc

--- lightsout_06_clock_class.ino
+++ lightsout_06_clock_class.ino
@@ -64,6 +64,27 @@ public:
     weekday = now.dayOfWeek();
   }
 
+  int moon_phase(int year, int month, int day) {
+    /* Calculates the moon phase (0-7), accurate to 1 segment.
+       0 = > new moon.
+       4 => Full moon. */
+    int g, e;
+
+    if (month == 1) --day;
+    else if (month == 2) day += 30;
+    else { // m >= 3
+      day += 28 + (month-2)*3059/100;
+      // adjust for leap years
+      if (!(year & 3)) ++day;
+      if ((year%100) == 0) --day;
+    }
+
+    g = (year-1900)%19 + 1;
+    e = (11*g + 18) % 30;
+    if ((e == 25 && g > 11) || e == 24) e++;
+    return ((((e + day)*6+11)%177)/22 & 7);
+  }
+
   void update_lights() {
     // set all colors to off
     for (index = 0; index < 16; index++)
